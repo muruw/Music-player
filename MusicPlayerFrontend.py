8be0adbce187417f2d2ae68fb52633ee13cd5085
Marcus Murumäe <35424371+muruw@users.noreply.github.com>
2018-10-11 19:14:55 +0300

    Add files via upload
    Added some additional comments

--- MusicPlayerFrontend.py
+++ MusicPlayerFrontend.py
@@ -1,5 +1,9 @@
 import MusicPlayerBackend
-
+"""
+import filename
+MusicPlayerBackend for short is mpb
+Use mpb to access the functions in MusicPlayerBackend.py
+"""
 mpb = MusicPlayerBackend.MusicPlayerBackend()
 
 print(mpb.writeHello("Tere külalised!"))
