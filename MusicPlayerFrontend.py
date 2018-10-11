d2f2d4b45ecd00d3aa23cb05a9100e50a7be780c
Rando L <32828239+intermetric@users.noreply.github.com>
2018-10-11 19:53:26 +0300

    Update MusicPlayerFrontend.py

--- MusicPlayerFrontend.py
+++ MusicPlayerFrontend.py
@@ -3,6 +3,7 @@ import MusicPlayerBackend
 import filename
 MusicPlayerBackend for short is mpb
 Use mpb to access the functions in MusicPlayerBackend.py
+suck on my balls.pdf
 """
 mpb = MusicPlayerBackend.MusicPlayerBackend()
 
