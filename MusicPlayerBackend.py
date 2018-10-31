019451903a9a5da04ac07910199cbae6f0feb5cc
muruw <murumaem@gmail.com>
2018-10-31 17:13:07 +0200

    Created a new class called Database, which holds two new functions - DatabaseSetup and DatabaseInsertFile.

--- MusicPlayerBackend.py
+++ MusicPlayerBackend.py
@@ -1,6 +1,9 @@
 from kivy.core.audio import SoundLoader
 
+import sqlite3
+
 from pathlib import Path
+
 
 class MusicPlayerBackend:
 
@@ -8,26 +11,27 @@ class MusicPlayerBackend:
     soundFile = ""
     soundFilePath = ""
 
-    def __init__(self):
-        pass
-    
     def soundFilePlay(self):
         soundName = self.soundFilePath
         try:
+            self.soundFile = Path(soundName.source).parts[-1]
+
             print("Sound found at %s" % soundName.source)
             print("Sound is %.3f seconds" % soundName.length)
-            self.soundFile = (Path(soundName.source).parts)[-1]
+
             # The sound plays til it's stopped 
             soundName.loop = True
             soundName.play()
             print("Playfile: " + str(soundName.state))
+
         except:
             pass
-        
-    
+
+
+
     def soundFilePause(self):
         sound = self.soundFilePath
-        
+
         # We must pass the error if sound == "", otherwise the application would crash
         try:
             if sound.state == "play":
@@ -35,21 +39,45 @@ class MusicPlayerBackend:
                 print("SoundFilePause() " + sound.state)
         except:
             pass
-        
+
     def soundFileLoader(self, soundName):
-        
         # This function will preload the given song
-        self.soundFilePath = SoundLoader.load(soundName)
-        
-        return self.soundFile
+        if self.soundFilePath == "":
+            self.soundFilePath = SoundLoader.load(soundName)
+        else:
+            self.soundFilePath.state = "stop"
+            self.soundFilePath = SoundLoader.load(soundName)
+            print("State = play")
 
-    
 
 
-        
-    
-        
-        
-            
-            
-        
+class Database:
+
+
+
+    def __init__(self):
+        self.databaseConnection = sqlite3.connect("MusicPlayerDatabase.db")
+        self.cursor = self.databaseConnection.cursor()
+
+    def DatabaseSetup(self):
+
+        try:
+            self.databaseConnection.execute(
+                "CREATE TABLE IF NOT EXISTS MusicFiles (ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+                "fileLocation TEXT, fileName TEXT);")
+
+            self.databaseConnection.commit()
+            print("Database has been created INFO 01: DatabaseSetup()")
+
+        except:
+            print("Database was not found ERROR 01: DatabaseSetup()")
+
+    def DatabaseInsertFile(self, fileLocation, fileName):
+
+
+        self.databaseConnection.execute("INSERT INTO MusicFiles(fileLocation, fileName) VALUES ('" +
+                                        fileLocation + "', '" +
+                                        fileName + "')")
+        print(MusicPlayerBackend.soundFilePath)
+        print("Commit successful INFO 02: DatabaseInsertFile()")
+        self.databaseConnection.commit()
--- MusicPlayerFrontend.py
+++ MusicPlayerFrontend.py
@@ -1,79 +1,81 @@
 import kivy
+import MusicPlayerBackend
+
+# Use mpb to access backend methods
+mpb = MusicPlayerBackend.MusicPlayerBackend()
+db = MusicPlayerBackend.Database()
+
 kivy.require("1.10.0")
 
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.button import Button
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.properties import StringProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.filechooser import FileChooserListView
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.anchorlayout import AnchorLayout
 
 from kivy.core.window import Window
 
 Window.clearcolor = (1, 1, 1, 1)
 
-# Use mpb to access backend methods
-import MusicPlayerBackend
-mpb = MusicPlayerBackend.MusicPlayerBackend()
-
 # Locating all the .kv files
 from os import listdir
+
 kv_path = "./kivyFiles/"
 for kv in listdir(kv_path):
     print(kv)
     Builder.load_file(kv_path + kv)
-    
 
+# We will initialize the database
+db.DatabaseSetup()
 
 class MainMenuScreen(Screen):
     pass
 
+
 class MusicScreen(Screen):
     soundName = StringProperty()
-    
+
     def playMusic(self):
-        # At the moment it is hardcoded but later we will use
-        # the fileChooser to choose the song
+        #
         mpb.soundFilePlay()
         print(self.soundName)
         self.soundNameChange()
-        
-        
+
+        # Adding the song to the playlist
+        db.DatabaseInsertFile(str(mpb.soundFilePath), mpb.soundFile)
+
     def pauseMusic(self):
         # Pause the sound currently being played
         mpb.soundFilePause()
-        
+
     def soundNameChange(self):
         # This function changes the label to the song
         # we are playing at that moment
         self.soundName = mpb.soundFile
-        
 
 
 class FileChooserScreen(Screen):
     # File we are going to play
     soundFile = ""
-    
+
     def selectFile(self, *args):
         try:
-            self.label.text = args[1][0]          
+            self.label.text = args[1][0]
             mpb.soundFileLoader(args[1][0])
-            
         except:
             pass
-    
+
+
 sm = ScreenManager()
-sm.add_widget(MainMenuScreen(name = "main_menu"))
-sm.add_widget(MusicScreen(name = "music"))
-sm.add_widget(FileChooserScreen(name = "file_chooser"))
+sm.add_widget(MainMenuScreen(name="main_menu"))
+sm.add_widget(MusicScreen(name="music"))
+sm.add_widget(FileChooserScreen(name="file_chooser"))
+
 
 class MusicPlayerFrontendApp(App):
     def build(self):
         self.title = "Music Player"
         return sm
-    
+
+
 if __name__ == "__main__":
     MusicPlayerFrontendApp().run()
-
